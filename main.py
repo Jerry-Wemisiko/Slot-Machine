9723a41244aec8d2988bd2c14a881c60b070bea2
Jerry-Wemisiko <jerrywemisiko@gmail.com>
2022-10-04 02:06:19 +0300

    function to enter deposit

---
+++ main.py
@@ -0,0 +1,17 @@
+ 
+from curses.ascii import isdigit
+
+
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+
+        if amount.isdigit():
+            amount = int(amount)
+            if amount> 0:
+                break
+            else:
+                print("Amount must be greater than 0")
+
+        else:
+            print("Please enter an amount")
